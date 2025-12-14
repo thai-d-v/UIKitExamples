5e9d3a57f21a572546d8b52e4ee029495367eb70
Duong Thai <dvthai.dev@gmail.com>
2025-12-14 15:42:40 +0700

    basic components of collection view

--- UIKitExample/UIKitExample/ViewController.swift
+++ UIKitExample/UIKitExample/ViewController.swift
@@ -12,11 +12,21 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    // The Collection View is a scrollable view frame
     let collectionView = UICollectionView(
       frame: .zero,
       collectionViewLayout: UICollectionViewFlowLayout()
     )
-    collectionView.backgroundColor = .red
+
+    // Data source
+    collectionView.dataSource = self
+
+    // UI Presentation
+    // 1. layout
+    // 2. cell
+    collectionView.delegate = self
+    collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "CELL")
+
     self.view.addSubview(collectionView)
 
     collectionView.translatesAutoresizingMaskIntoConstraints = false
@@ -26,6 +36,25 @@ class ViewController: UIViewController {
       collectionView.leftAnchor.constraint(equalTo: self.view.leftAnchor),
       collectionView.rightAnchor.constraint(equalTo: self.view.rightAnchor),
     ])
+  }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    return .init(width: collectionView.frame.width/4, height: 200)
+  }
+}
+
+extension ViewController: UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    30
+  }
+
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CELL", for: indexPath)
+    cell.backgroundColor = .systemBlue
+
+    return cell
   }
 }
 
