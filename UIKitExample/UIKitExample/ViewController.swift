2182f482c2ca12a09d609e9001099cf25343567d
Duong Thai <dvthai.dev@gmail.com>
2025-12-14 13:15:33 +0700

    set up view frame by AutoLayout

--- UIKitExample/UIKitExample/ViewController.swift
+++ UIKitExample/UIKitExample/ViewController.swift
@@ -13,55 +13,19 @@ class ViewController: UIViewController {
     super.viewDidLoad()
 
     let collectionView = UICollectionView(
-      frame: view.window?.windowScene?.screen.bounds ?? .zero,
+      frame: .zero,
       collectionViewLayout: UICollectionViewFlowLayout()
     )
     collectionView.backgroundColor = .red
     self.view.addSubview(collectionView)
 
-    print(
-      #function,
-      "view.window?.windowScene?.screen.bounds:",
-      String(describing: view.window?.windowScene?.screen.bounds)
-    )
-  }
-
-  override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
-
-    print(
-      #function,
-      "view.window?.windowScene?.screen.bounds:",
-      String(describing: view.window?.windowScene?.screen.bounds)
-    )
-  }
-
-  override func viewIsAppearing(_ animated: Bool) {
-    super.viewIsAppearing(animated)
-
-    print(
-      #function,
-      "view.window?.windowScene?.screen.bounds:",
-      String(describing: view.window?.windowScene?.screen.bounds)
-    )
-  }
-
-  override func viewWillLayoutSubviews() {
-    super.viewWillLayoutSubviews()
-    print(
-      #function,
-      "view.window?.windowScene?.screen.bounds:",
-      String(describing: view.window?.windowScene?.screen.bounds)
-    )
-  }
-
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
-    print(
-      #function,
-      "view.window?.windowScene?.screen.bounds",
-      String(describing: view.window?.windowScene?.screen.bounds)
-    )
+    collectionView.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      collectionView.topAnchor.constraint(equalTo: self.view.topAnchor),
+      collectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+      collectionView.leftAnchor.constraint(equalTo: self.view.leftAnchor),
+      collectionView.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+    ])
   }
 }
 
